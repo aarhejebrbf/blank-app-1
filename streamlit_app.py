e63929c6364fb0f50e483471af528155af9cc7b4
aarhejebrbf <bcsje002@naver.com>
2025-05-09 09:21:13 +0000

    메인 제목입니다

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,11 @@
 import streamlit as st
 
-st.title("🎈 My new app")
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
+st.title("🎈 노승우 프로젝트")
+st.info(
+    "안녕하세요. 반갑습니다"
 )
+
+# 페이지 구조용 제목 출력
+st.title("메인 제목입니다")
+st.header("중간 제목입니다")
+st.subheader("하위 제목입니다")
